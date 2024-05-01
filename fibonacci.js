1a2b832da07af0ed2b6e85dcc40fcfb1ec1cacd2
Sanup Divakaran <sanukyr@gmail.com>
2024-05-01 15:55:23 +0530

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+  const array = [0, 1];
+  for (let i = 2; i < n; i++) {
+    //array[i] = array[i - 1] + array[i - 2];
+    array.push(array[i - 1] + array[i - 2]);
+  }
+  return array;
+}
+
+console.log(fibonacci(5));
