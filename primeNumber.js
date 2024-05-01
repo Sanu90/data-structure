c4685327aa5413b29b14b1de40638fd6e07f0f13
Sanup Divakaran <sanukyr@gmail.com>
2024-05-01 19:23:30 +0530

    .\primeNumber.js

---
+++ primeNumber.js
@@ -0,0 +1,15 @@
+// math algorithm 3 //
+
+function primeNumber(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(primeNumber(101));
+
+// time complexity (worst case) is: O(n)
