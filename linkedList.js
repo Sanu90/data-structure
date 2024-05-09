685149c3e9e5abff0aa25321f0ebb9fcb751351c
Sanup Divakaran <sanukyr@gmail.com>
2024-05-09 10:31:31 +0530

    linkedlist zero node

--- linkedList.js
+++ linkedList.js
@@ -1,1 +1,26 @@
-console.log("this is a linked list");
+// create a zero node linkedlist //
+
+class node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size == 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const list = new linkedList();
+console.log(list.isEmpty());
+console.log(list.getSize());
