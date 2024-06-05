b78bd1d9945908b8ee1601a4e58bc3b58ed176a7
Sanup Divakaran <sanukyr@gmail.com>
2024-06-05 19:21:00 +0530

    prime v2

--- primeNumber.js
+++ primeNumber.js
@@ -13,3 +13,15 @@ function primeNumber(n) {
 console.log(primeNumber(60));
 
 // time complexity (worst case) is: O(n)
+
+// using recursion //
+
+function isPrime(num,divisor=2){
+  if(num<2) return false;
+  if(num%divisor===0) return false;
+  if(divisor>Math.sqrt(num)) return true;
+  
+  return isPrime(num,divisor+1)
+}
+
+console.log(isPrime(33))
