0f3a3e017e23e1152a360d99a363d06c68b4989a
Sanup Divakaran <sanukyr@gmail.com>
2024-06-24 23:20:20 +0530

    quick sort

---
+++ quickSort.js
@@ -0,0 +1,20 @@
+let arr = [10, 1, 19, 0, 16, 5, 14];
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort(arr));
